11f9c0203f17b0b1d2016047a62df7b2ef35a1c3
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2021-11-04 16:14:14 +0100

    Rename functions

--- scripts/test_utils.py
+++ scripts/test_utils.py
@@ -1,32 +1,6 @@
 import pytest
 
-from padding import add_padding, extract_hit_region
-
-# These are the test cases for aligning seq1 to seq2 by using padding. This
-# assumes that seq2 has already been blasted against seq1, which is the source
-# of start1, end1 and start2, end2
-padding_cases = [
-        # Test case where both seq1 and seq2 are identical, and fully match
-        ('AAAAA', 1, 5, 'AAAAA', 1, 5, 'AAAAA'),
-        # Test case where both seq1 and seq2 are the same length, but seq1
-        # overhangs the beginning
-        ('GGAAA', 3, 5, 'AAAAA', 3, 5, 'GGAAA'),
-        # Test case where both seq1 and seq2 are the same length, but seq1
-        # overhangs the end
-        ('AAAGG', 1, 3, 'AAAAA', 1, 3, 'AAAGG'),
-        # Test case where seq1 is longer, and the beginning of the sequence
-        # matches
-        ('AAAAAGG', 1, 5, 'AAAAA', 1, 5, 'AAAAA'),
-        # Test case where seq1 is longer, and the end of the sequence matches
-        ('GGAAAAA', 3, 7, 'AAAAA', 1, 5, 'AAAAA'),
-        # Test case where seq1 is longer, but only the center of the sequence
-        # matches
-        ('GGGAAAGGG', 4, 6, 'AAAAA', 2, 4, 'GAAAG'),
-        # Test case where seq1 is shorter, and must be padded at the beginning
-        ('AAA', 1, 3, 'GGAAA', 3, 5, '--AAA'),
-        # Test case where seq1 is shorter, and must be padded at the end
-        #('AAA', 1, 3, 'AAAGG', 1, 3, 'AAA--'),
-]
+from utils import extract_hit_region
 
 # These are the test cases for extracting regions of seq2 from seq1. This
 # assumes that seq2 has already been blasted against seq1, which is the source
@@ -55,10 +29,6 @@ extract_cases = [
         # full lenght of seq1
         ('GGGGGAAAAA', 6, 10, 'CCCCAAAAAAAAAA', 10, 14, 'GGGGGAAAAA'),
 ]
-
-@pytest.mark.parametrize('seq1,start1,end1,seq2,start2,end2,expected', padding_cases)
-def test_add_padding(seq1, start1, end1, seq2, start2, end2, expected):
-    assert add_padding(seq1, start1, end1, seq2, start2, end2) == expected
 
 @pytest.mark.parametrize('seq1,start1,end1,seq2,start2,end2,expected', extract_cases)
 def test_extract_hit_region(seq1, start1, end1, seq2, start2, end2, expected):
---
+++ scripts/utils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+def extract_hit_region(seq1, start1, end1, seq2, start2, end2):
+    """ Extract the hit region for seq2 from seq1.
+
+        Briefly, this means that if part of seq1 is a blast hit for seq2, we
+        extend the region around this hit for seq1 untill it is the same size
+        as seq2.
+
+        This way, we can fin insertions or deletions in the assembly where the
+        target gene should be, which are otherwise missed by BLAST.
+    """
+    # How much of the beginning of seq2 is missing from the blast hit
+    extended_begin = start1 - min(start1, start2)
+
+    # How much of the end of seq2 is missing from the blast hit
+    extended_end = end1 + len(seq2) - end2
+
+    # Return the slice of seq1 that contains both the original hit and the
+    # extension
+    return seq1[extended_begin:extended_end]
