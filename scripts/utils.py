2be87c9384ee8f874b1725f1e13d662b56f6ee12
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2021-11-18 09:55:19 +0100

    Fix typo

--- scripts/utils.py
+++ scripts/utils.py
@@ -4,10 +4,10 @@ def extract_hit_region(seq1, start1, end1, seq2_length, start2, end2):
     """ Extract the hit region for seq2 from seq1.
 
         Briefly, this means that if part of seq1 is a blast hit for seq2, we
-        extend the region around this hit for seq1 untill it is the same size
+        extend the region around this hit for seq1 until it is the same size
         as seq2.
 
-        This way, we can fin insertions or deletions in the assembly where the
+        This way, we can find insertions or deletions in the assembly where the
         target gene should be, which are otherwise missed by BLAST.
     """
     # How much of the beginning of seq2 is missing from the blast hit
